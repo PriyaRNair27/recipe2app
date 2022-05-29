56a7fc2aa3ec546775d02ef83a06c4c2cb09086f
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-29 12:19:36 +0530

    search

---
+++ src/Components/Recipesearch.js
@@ -0,0 +1,86 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+
+const Recipesearch = () => {
+    const [data,setdata]=useState("")
+    const [recipetitle,setrecipetitle]=useState([{"category":"","description":"","preapredby":""}])
+    const subdata=()=>{
+        const data={"recipetitle":recipetitle}
+        console.log(data)
+        axios.post("http://localhost:4000/api/recipsearch",data).then((response)=>{
+            console.log(response.data)
+        })
+    }
+    const deleteApiCall=(id)=>{
+        const data={"_id":id}
+       console.log(data)
+       axios.post("http://localhost:4000/api/recipdelete",data).then((response)=>
+       {
+           if(response.data.status=="success")
+           {
+               alert("success")
+           }
+           else
+           {
+               alert("error")
+           }
+       })
+      
+    }
+    
+
+        return (
+    <div>
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">RECIPE TITLE</label>
+                    <input onChange={(e)=>{setrecipetitle(e.target.value)}}  type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button  onClick={subdata} className="btn btn-danger">SEARCH</button>
+                </div>
+            </div>
+
+            
+            {data.map((value,key)=>
+    {
+        return<div className="row g-3">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    CATEGORY:
+                <input type="text" value={value.category} className="form-control"/>
+                </div>
+
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                DESCRIPTION:
+                <input type="text" value={value.description} className="form-control"/>
+                </div>
+
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                PREPARED BY:
+                <input type="text" value={value.preparedby} className="form-control"/>
+                </div>
+
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+    <button   onClick={()=>{deleteApiCall(value._id)}} className="bt btn-success">DELETE</button>
+
+    </div>
+                
+              
+                
+                </div>
+
+                  
+
+    })}
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default Recipesearch
